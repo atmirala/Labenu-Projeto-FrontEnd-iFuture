f0e0cfb0373bfe59f126c623cfee87b6348a33eb
Rosana Rezende <contato@rosanarezende.com>
2020-07-01 14:38:53 -0300

    organização do repositório

--- app/src/actions/user.js
+++ app/src/actions/user.js
@@ -6,15 +6,15 @@ import { routes } from '../containers/Router'
 export const signup = (signupData) => async (dispatch) => {
     try {
         const response = await axios.post(`${baseUrl}/signup`, signupData)
-        const token = response.data.token
+        const token = response?.data?.token
         localStorage.setItem("token", token)
-        const hasAddress = response.data.user.hasAddress
-        if(response.data.user.hasAddress){
+        const hasAddress = response?.data?.user?.hasAddress
+        if(response?.data?.user?.hasAddress){
             localStorage.setItem('hasAddress', hasAddress)
         }
         dispatch(push(routes.adressRegister))
     } catch (error) {
-        console.error(error.message)
+        console.error(error?.message)
         // alert("Não foi possivel criar seu cadastro, tente novamente mais tarde !")
     }
 }
@@ -28,13 +28,13 @@ export const addAdress = (addAdressData) => async (dispatch) => {
                     auth: getToken()
                 }
             })
-        const token = response.data.token
-        const hasAddress = response.data.user.hasAddress
+        const token = response?.data?.token
+        const hasAddress = response?.data?.user?.hasAddress
         localStorage.clear()
         localStorage.setItem("token", token)
         localStorage.setItem('hasAddress', hasAddress)
     } catch (error) {
-        console.error(error.message)
+        console.error(error?.message)
         // alert("Não foi possivel cadastrar esse endereço, tente novamente mais tarde!")
     }
 }
@@ -42,8 +42,8 @@ export const addAdress = (addAdressData) => async (dispatch) => {
 export const login = loginData => async (dispatch) => {
     try {
         const response = await axios.post(`${baseUrl}/login`, loginData)
-        const token = response.data.token;
-        const hasAddress = response.data.user.hasAddress
+        const token = response?.data?.token;
+        const hasAddress = response?.data?.user?.hasAddress
         localStorage.setItem("token", token)
         if(response.data.user.hasAddress){
             localStorage.setItem('hasAddress', hasAddress)
@@ -53,7 +53,7 @@ export const login = loginData => async (dispatch) => {
             alert('É necessário preencher os dados do endereço para continuar')
         }
     } catch (erro) {
-        console.error(erro.message)
+        console.error(erro?.message)
         alert("Não foi possivel fazer o login, tente novamente mais tarde!")
     }
 }
@@ -72,7 +72,7 @@ export const getProfile = () => async (dispatch) => {
                 auth: getToken()
             }
         })
-        dispatch(setProfile(response.data.user))
+        dispatch(setProfile(response?.data?.user))
     } catch (error) {
         console.error(error.message)
         alert("Não foi possível acessar os dados do usuário.")
@@ -109,10 +109,10 @@ export const updateProfile = dataUpdateProfile => async dispatch => {
             }
         })
 
-        dispatch(setProfile(response.data.user))
+        dispatch(setProfile(response?.data?.user))
         dispatch(push(routes.profile))
     } catch(error) {
-        console.error(error.message)
+        console.error(error?.message)
         alert("Não foi possível atualizar os dados do usuário.")
     }
 }
